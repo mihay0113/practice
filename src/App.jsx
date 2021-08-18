7a66d6766c794a54a190d94ff1ff956dd19c3987
Имя Фамилия <mihay0113@gmail.com>
2021-08-18 15:10:35 +0300

    make a custom hook localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import './App.scss';
 
-export const App = () => (
-  <div className="starter">
-    React starter pack
-  </div>
-);
+const useLocaleStorage = (key, initialValue) => {
+  const [count, setCount] = useState(
+    JSON.parse(localStorage.getItem(key) || initialValue),
+  );
+
+  const save = (value) => {
+    setCount(value);
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [count, save];
+};
+
+export const App = () => {
+  const [count, setCount] = useLocaleStorage('count', 0);
+
+  const increase = () => setCount(count + 1);
+  const decrease = () => setCount(count - 1);
+
+  const Container = styled.div`
+    margin: 100px;
+  `;
+  const Button = styled.button`
+    width: 50px;
+    height: 50px;
+    background-color: ${count > 3 || count === 1 ? '#a52a2a' : '#ff8c00'};
+    margin: 50px;
+  `;
+
+  const Count = styled.span`
+    font-size: 40px;
+    color: ${count > 3 ? '#0000ff' : '#8a2be2'}
+  `;
+
+  return (
+    <Container className="App">
+      <Button type="button" onClick={decrease}>-</Button>
+      <Count>{count}</Count>
+      <Button type="button" onClick={increase}>+</Button>
+    </Container>
+  );
+};
